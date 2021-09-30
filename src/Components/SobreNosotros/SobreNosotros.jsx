0192c37315ee66412771097426795c692f970871
Antsar87 <jomay1129@gmail.com>
2021-09-29 20:59:09 -0600

    mejoras

--- src/Components/SobreNosotros/SobreNosotros.jsx
+++ src/Components/SobreNosotros/SobreNosotros.jsx
@@ -3,26 +3,59 @@ import styled from 'styled-components';
 import Fade from 'react-reveal/Fade';
 import Bounce from 'react-reveal/Bounce';
 
-import Logo from './Elementos-06.jpg';
+import Background from './Elementos-16.jpg';
+import Img from './Elementos-17.jpg';
 
 const Container = styled.div`
   margin-top: 30px;
   display: flex;
   align-items: center;
-  background: url(${Logo});
+  background-image: url(${Background});
   background-repeat: no-repeat;
-  height: 336px;
+  height: 300px;
   width: 100%;
-  /* transform: scale(1.4); */
+  background-position: center;
+  background-size: cover;
+  min-height: 300px;
+  position: relative;
+  z-index: 1;
+
+  &:hover img {
+    transform: rotate(-5deg);
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(#ffa600ec, #ffa600ec);
+    z-index: -1;
+  }
 `;
 
-const Box = styled.div`
-  width: 70%;
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 30px;
+  width: 100%;
+  height: 100%;
+  gap: 30px;
+`;
+
+const Foto = styled.img`
+  width: 300px;
+  height: 250px;
+  /* border: 3px solid white; */
+  transition: all 1s ease-in-out;
+  outline: 10px white solid;
 `;
 
 const Text = styled.div`
   color: white;
-  width: 90%;
+  height: 100%;
 
   & h1 {
     text-transform: uppercase;
@@ -36,21 +69,23 @@ const Text = styled.div`
 const SobreNosotros = () => {
   return (
     <Container>
-      <Box></Box>
-      <Fade right>
-        <Text>
-          <h1>Quienes Somos</h1>
-          <Bounce right cascade>
-            <p>
-              Palabra Fiel es una gran familia, deseamos rescatar los valores
-              espirituales y morales que Dios establece en su Palabra. Queremos
-              vivir en amor y en armonía los unos con los otros como en el
-              inicio de la iglesia primitiva, todos unánimes, en un mismo
-              sentir.
-            </p>
-          </Bounce>
-        </Text>
-      </Fade>
+      <Flex>
+        <Foto src={Img} />
+        <Fade right>
+          <Text>
+            <h1>Quienes Somos</h1>
+            <Bounce right cascade>
+              <p>
+                Palabra Fiel es una gran familia, deseamos rescatar los valores
+                espirituales y morales que Dios establece en su Palabra.
+                Queremos vivir en amor y en armonía los unos con los otros como
+                en el inicio de la iglesia primitiva, todos unánimes, en un
+                mismo sentir.
+              </p>
+            </Bounce>
+          </Text>
+        </Fade>
+      </Flex>
     </Container>
   );
 };
